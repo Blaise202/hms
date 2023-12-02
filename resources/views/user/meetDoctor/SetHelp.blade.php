359801f1e3b2a6d0f41fde69704ae20a777ba5e0
Blaise202 <147907220+Blaise202@users.noreply.github.com>
2023-12-02 17:05:47 +0200

    stetting help options

--- resources/views/user/meetDoctor/SetHelp.blade.php
+++ resources/views/user/meetDoctor/SetHelp.blade.php
@@ -1,26 +1,18 @@
 <div class="page-section">
     <div class="container">
         <h2 class="text-center mb-5 wow fadeInUp">How do you prefer to be helped</h2>
-        <div class="page-section">
-            <div class="container">
-                <div class="owl-carousel wow fadeInUp" id="doctorSlideshow">
-                    
-                    <nav class="navbar navbar-expand-lg navbar-light shadow-sm col-sm-12">
-                        <div class="container col-sm-12 text-right-flex">
-                            <div class="col-sm-10 text-right" id="navbarSupport">
-                                <ul class="navbar-nav ml-auto">
-                                    <li class="nav-item">
-                                        <a class="btn btn-primary ml-lg-3" href="{{route('login')}}">Login</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="btn btn-primary ml-lg-3" href="{{route('register')}}">Register</a>
-                                    </li>
-                                </ul>
-                            </div> <!-- .navbar-collapse -->
-                        </div> <!-- .container -->
-                    </nav>
-                </div>
-            </div>
-        </div>         
+            <nav class="navbar navbar-expand-lg navbar-light col-sm-12">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ml-lg-4">
+                        <a class="btn btn-primary ml-lg-4 " href="{{url('guideTexts')}}">Read a guide</a>
+                    </li>
+                    <li class="nav-item ml-lg-4">
+                        <a class="btn btn-primary ml-lg-4" href="{{url('guideVideos')}}">Watch a video</a>
+                    </li>
+                    <li class="nav-item ml-lg-4">
+                        <a class="btn btn-primary ml-lg-4" href="{{url('doctorsContacts')}}">Call a doctor</a>
+                    </li>
+                </ul>  
+            </nav>        
     </div>
 </div>
